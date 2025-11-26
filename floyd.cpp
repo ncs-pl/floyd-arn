b9cbbea20858f090e07d05e055a64e79828039a9
Nicolas Paul <nicolas.paul1@etu.univ-orleans.fr>
2025-11-26 17:56:39 +0100

    Blocked Floyd-Warshall finally done

--- floyd.cpp
+++ floyd.cpp
@@ -29,76 +29,63 @@
 #include <mpi.h>
 
 constexpr int kInfinity = std::numeric_limits<int>::max();
+constexpr int kTrue = 1;
+constexpr int kFalse = 0;
 
 int
 main(int argc, char **argv)
 {
-  int status = EXIT_SUCCESS;
+  int status = 0; // pas EXIT_SUCCESS...
+  int i, j, k; // itérateurs de boucle
 
-  int *A = nullptr;
-  int *D = nullptr;
-  int *Q = nullptr;
+  int pid, nprocs;
+  int root;
+  int b; // longueur d'un bloc
   char* input = nullptr;
+  int *A = nullptr;
+
+  FILE* fd = nullptr; // file descriptor d'input
+  std::map<std::string, int> index;
+  Agraph_t *G;
+  Agnode_t *u, *v;
+  Agedge_t *e;
+
+  int *D = nullptr;
+  int *KC = nullptr; // colonne pivots
+  int *KR = nullptr; // ligne pivots
+
+  int n; // nombre de sommets.
+  int q; // nombre de blocs
+
+  MPI_Comm ccart, crow, ccol;
+  int *co = nullptr; // (ligne, colonne)
+  int *dims = nullptr;
+  int *pds = nullptr;
 
   // Récupération des arguments du programme.
 
   MPI_Init(&argc, &argv);
 
-  int pid, nprocs;
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-  if (argc != 4)
-  {
+  if (argc != 4) {
     std::cout << "usage: " << argv[0] << " <root> <b> <input>" << std::endl;
     status = EXIT_FAILURE;
     goto cleanup;
   }
 
-  int root;
-  try
-  {
-    root = std::stoi(argv[1], nullptr, 10);
+  root = std::stoi(argv[1], nullptr, 10);
 
-    if (root < 0 || root >= nprocs) {
-      std::cerr << argv[0] << ": root doit être un processeur valide" << std::endl;
-      status = EXIT_FAILURE;
-      goto cleanup;
-    }
-  }
-  catch (std::invalid_argument const& _ex)
-  {
-    std::cerr << argv[0] << ": root doit être un entier" << std::endl;
-    status = EXIT_FAILURE;
-    goto cleanup;
-  }
-  catch (std::out_of_range const& _ex)
-  {
-    std::cerr << argv[0] << ": root doit être une valeur raisonnable" << std::endl;
+  if (root < 0 || root >= nprocs) {
+    std::cerr << argv[0] << ": root doit être un processeur valide" << std::endl;
     status = EXIT_FAILURE;
     goto cleanup;
   }
 
-  int b;
-  try
-  {
-    b = std::stoi(argv[2], nullptr, 10);
-
-    if (b <= 0) {
-      std::cerr << argv[0] << ": b doit être positif" << std::endl;
-      status = EXIT_FAILURE;
-      goto cleanup;
-    }
-  }
-  catch (std::invalid_argument const& _ex)
-  {
-    std::cerr << argv[0] << ": b doit être un entier" << std::endl;
-    status = EXIT_FAILURE;
-    goto cleanup;
-  }
-  catch (std::out_of_range const& _ex)
-  {
-    std::cerr << argv[0] << ": b doit être une valeur raisonnable" << std::endl;
+  b = std::stoi(argv[2], nullptr, 10);
+  if (b <= 0) {
+    std::cerr << argv[0] << ": b doit être positif" << std::endl;
     status = EXIT_FAILURE;
     goto cleanup;
   }
@@ -107,83 +94,42 @@ main(int argc, char **argv)
 
   // Initialisation de la matrice A.
 
-  int n;
 
-  if (pid == root)
-  {
+  if (pid == root) {
     // Lecture et compilation.
 
-    FILE *fd = std::fopen(input, "r");
-    if(!fd)
-    {
-      std::cerr << argv[0] << ": \"" << input << "\" ne peut pas être ouvert" << std::endl;
-      status = EXIT_FAILURE;
-      goto cleanup;
-    }
-
-    Agraph_t *G = agread(fd, nullptr);
-    if (!G)
-    {
-      std::cerr << argv[0] << ": \"" << input << "\" ne peut pas être compilé" << std::endl;
-      status = EXIT_FAILURE;
-      goto cleanup;
-    }
-
+    fd = std::fopen(input, "r");
+    G = agread(fd, nullptr);
     fclose(fd);
 
     n = agnnodes(G);
-    std::map<std::string, int> index;
 
-    int t = 0;
+    i = 0;
     for (Agnode_t *u = agfstnode(G); u; u = agnxtnode(G, u))
     {
-      index.emplace(agnameof(u), t++);
+      index.emplace(agnameof(u), i++);
     }
 
     // Initialisation de A à l'infini sauf sur les diagonales.
 
     A = new int[n*n];
 
-    for (int i = 0; i < n*n; i++)
-    {
+    for (i = 0; i < n*n; i++) {
       A[i] = kInfinity;
     }
 
-    for (int i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
       A[i*n+i] = 0;
     }
 
     // Construction de la matrice adjacente.
 
-    for (Agnode_t *u = agfstnode(G); u; u = agnxtnode(G, u))
-    {
-      int const i = index[agnameof(u)];
-      for (Agedge_t *e = agfstout(G, u); e; e = agnxtout(G, e))
-      {
-        Agnode_t *v = aghead(e);
-        int const j = index[agnameof(v)];
-
-        int w;
-        try
-        {
-	  std::string attr = agget(e, (char*)"weight");
-          w = std::stoi(attr, nullptr, 10);
-        }
-        catch (std::invalid_argument const& _ex)
-        {
-          std::cerr << argv[0] << ": le weight d'une arête doit être un entier" << std::endl;
-          status = EXIT_FAILURE;
-          goto cleanup;
-        }
-        catch (std::out_of_range const& _ex)
-        {
-          std::cerr << argv[0] << ": le weight d'une arête doit être une valeur raisonnable" << std::endl;
-          status = EXIT_FAILURE;
-          goto cleanup;
-        }
-
-        A[i*n+j] = A[j*n+i] = w;
+    for (u = agfstnode(G); u; u = agnxtnode(G, u)) {
+      i = index[agnameof(u)];
+      for (e = agfstout(G, u); e; e = agnxtout(G, e)) {
+        v = aghead(e);
+        j = index[agnameof(v)];
+        A[i*n+j] = A[j*n+i] = std::stoi(agget(e, (char*)"weight"), nullptr, 10);
       }
     }
 
@@ -193,44 +139,71 @@ main(int argc, char **argv)
 
     std::cout << "Matrice adjacente :" << std::endl;
 
-    for (int i = 0; i < n; i++)
-    {
-      for (int j = 0; j < n; j++)
-      {
-        std::cout << "    " << A[i*n+j] << " ";
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
+        std::cout << "    "
+	          << (A[i*n+j] == kInfinity ? "∞" : std::to_string(A[i*n+j]))
+		  << " ";
       }
-
       std::cout << std::endl;
     }
   }
 
   // Découpage de A en blocs locaux D.
 
-  D = new int[b*b]; // hypothèses dans le sujet.
   MPI_Bcast(&n, 1, MPI_INT, root, MPI_COMM_WORLD);
+
+  D = new int[b*b]; // hypothèses dans le sujet.
   MPI_Scatter(A, b*b, MPI_INT, D, b*b, MPI_INT, root, MPI_COMM_WORLD);
+
+  // Création de la topologie cartésienne.
+
+  // TODO(nico): vérifier (n/b)^2 = nprocs
+  dims = new int[2];
+  dims[0] = dims[1] = n/b;
+
+  pds = new int[2];
+  pds[0] = pds[1] = kTrue;
+
+  MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, kTrue, &ccart);
+
+  co = new int[2];
+  MPI_Cart_coords(ccart, pid, 2, co);
+
+  MPI_Comm_split(ccart, co[0], co[1], &crow);
+  MPI_Comm_split(ccart, co[1], co[0], &ccol);
 
   // Algorithme de Floyd-Warshall.
 
-  Q = new int[n];
+  KR = new int[b];
+  KC = new int[b];
 
-  for (int l = 0; l < n; l++)
-  {
-    if (pid == l/b)
-    {
-      for (int i = 0; i < n; i++)
-      {
-        Q[i] = D[(l%b)*n+i];
+  for (k = 0; k < n; k++) {
+    // Identifier les pivots.
+
+    if (co[0] == k/b) {
+      for (i = 0; i < b; i++) {
+        KR[i] = D[(k%b)*b+i];
       }
     }
 
-    MPI_Bcast(Q, n, MPI_INT, l/b, MPI_COMM_WORLD);
+    if (co[1] == k/b) {
+      for (i = 0; i < b; i++) {
+        KC[i] = D[i*b+(k%b)];
+      }
+    }
 
-    for (int i = 0; i < b; i++)
-    {
-      for (int j = 0; j < n; j++)
-      {
-	D[i*n+j] = std::min(D[i*n+j], D[i*n+l] + Q[j]);
+    // Partage des pivots.
+
+    // TODO: comment bien décider du "root" ?
+    MPI_Bcast(KR, b, MPI_INT, ??, crow);
+    MPI_Bcast(KC, b, MPI_INT, ??, ccol);
+
+    // Mise à jour des blocs locaux.
+
+    for (i = 0; i < b; i++) {
+      for (j = 0; j < n; j++) {
+        D[i*b+j] = std::min(D[i*b+j], KC[i] + KR[j]);
       }
     }
   }
@@ -241,61 +214,33 @@ main(int argc, char **argv)
 
   // Affichage final.
 
-  if (pid == root)
-  {
+  if (pid == root) {
     std::cout << "Matrice finale :" << std::endl;
 
-    for (int i = 0; i < n; i++)
-    {
-      for (int j = 0; j < n; j++)
-      {
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
         std::cout << "    " << A[i*n+j] << " ";
       }
-
       std::cout << std::endl;
     }
   }
 
   // Nettoyage du programe.
 
- cleanup:
-   delete[] Q;
-   delete[] D;
-   delete[] A;
+cleanup:
+  delete[] coords;
+  delete[] periods;
+  delete[] dims;
+  delete[] KR;
+  delete[] KC;
+  delete[] D;
+  delete[] A;
+
+  MPI_Comm_free(&comm_col);
+  MPI_Comm_free(&comm_row);
+  MPI_Comm_free(&comm_cart);
 
   MPI_Finalize();
   return status;
 }
 
-#if 0
-  int qrows, qcols;
-  int *qrow = nullptr, *qcol = nullptr; // Données de pivots.
-  int q; // Coordonnée de ligne/colonne du bloc pivot.
-  int d; // Nouvelle valeur calculée pour un élément de D.
-
-  qrow = new int[b];
-  qcol = new int[b];
-  for (l = 0; l < n; ++l) {
-
-    // Déterminer les blocs aux valeurs pivots et les récupérer,
-    // ainsi que partager nos valeurs pivots aux autres.
-
-    q = l/b;
-    MPI_Bcast(qrow, b, MPI_INT, q*nb+q, MPI_COMM_WORLD);
-    MPI_Bcast(qcol, b, MPI_INT, q*nb+q, MPI_COMM_WORLD);
-
-    // Calculer D(l).
-
-    for (i = 0; i < b; ++i) {
-      for (j = 0; j < b; ++j) {
-        d = qrow[i] + qcol[j];
-	D[i*b+j] = d <= D[i*b+j] ? d : D[i*b+j];
-      }
-    }
-  }
-
-
-cleanup:
-  delete[] qrow;
-  delete[] qcol;
-#endif
